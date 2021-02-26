9d0130dbaf912c76e004ffdfc4fef0abcaba19cd
vrushabh1999 <vrushabh.jain@qualitykiosk.com>
2021-02-26 10:32:46 +0530

    new_Added

---
+++ vrushabh.java
@@ -0,0 +1,35 @@
+import java.util.List;
+
+public class ReadingCsv {
+    public static void main(String[] args) throws IOException, CsvException { 
+        Reader reader= Files.newBufferedReader(Paths.get("Student.txt"));
+        //parser!
+        CSVParser parser=new CSVParserBuilder()
+                .withSeparator('\t')
+                .build();
+
+
+        CSVReader object=new CSVReaderBuilder(reader)
+                .withCSVParser(parser)
+                .build();
+
+
+        //read the contents of the file!
+
+        List<String[]> data=object.readAll(); //returns a list of Strings from the file
+//3 ways for using data
+
+        for(int index=0;index<4;index++) {
+            System.out.println(data.get(index)[0]);// 0th line object
+        }
+
+
+        for(String[] everyElements: data){
+            System.out.println(everyElements[0]);
+        }
+
+        data.stream()
+                .forEach(everyElements-> System.out.println(everyElements[0]));
+    }
+
+}
