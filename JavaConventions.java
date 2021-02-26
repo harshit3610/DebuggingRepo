a83525640a821532ecfa70f641ec2fa2c6fd16b8
kajalshele <74015986+kajalshele@users.noreply.github.com>
2021-02-26 16:13:42 +0530

    New Changes are added

--- JavaConventions.java
+++ JavaConventions.java
@@ -19,20 +19,20 @@ public class ReadingCSV {
                 .withSeparator('\t')
                 .build();
 
-        CSVReader obj=new CSVReaderBuilder(reader)
+        CSVReader objectReader=new CSVReaderBuilder(reader)
                 .withCSVParser(parser)
                 .build();
 
         //read the contents of the file!
-        List<String[]> data=obj.readAll(); //returns a list of Strings from the file
+        List<String[]> data=objectReader.readAll(); //returns a list of Strings from the file
 
         //3 ways for using data
 
-        for(int i=0;i<4;i++) {
-           System.out.println(data.get(i)[0]);// 0th line object
+        for(int index=0;index<4;index++) {
+           System.out.println(data.get(index)[0]);// 0th line object
         }
 
-        for(String  arr[]: data){
+        for(String [] arr: data){
             System.out.println(arr[0]);
         }
 
