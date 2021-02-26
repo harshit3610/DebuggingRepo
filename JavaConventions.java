c39ccb707fae2e318c869946a320023eeab69b43
Aditikhodave99 <76159997+Aditikhodave99@users.noreply.github.com>
2021-02-26 15:46:39 +0530

    Update JavaConventions.java
    Conventions corrected, code updated

--- JavaConventions.java
+++ JavaConventions.java
@@ -1,4 +1,4 @@
-package OPENCSV;
+package OpenCSV;
 
 import com.opencsv.CSVParser;
 import com.opencsv.CSVParserBuilder;
@@ -6,42 +6,44 @@ import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
 import com.opencsv.exceptions.CsvException;
 
-import java.io.IOException;import java.io.Reader;import java.nio.file.Files;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
 public class ReadingCSV {
- public static void main(String[] args) throws IOException, CsvException {
+    public static void main(String[] args) throws IOException, CsvException {
         Reader reader= Files.newBufferedReader(Paths.get("Student.txt"));
 
         //parser!
-   CSVParser Parser=new CSVParserBuilder()
+        CSVParser parser=new CSVParserBuilder()
                 .withSeparator('\t')
                 .build();
 
 
-    CSVReader Obj1=new CSVReaderBuilder(reader)
+        CSVReader Obj1=new CSVReaderBuilder(reader)
                 .withCSVParser(parser)
-                			.build();
+                .build();
 
 
         //read the contents of the file!
 
-        		List<String[]> data=obj.readAll(); //returns a list of Strings from the file
-//3 ways for using data
+        List<String[]> data=Obj1.readAll(); //returns a list of Strings from the file
+                                           //3 ways for using data
 
-//        for(int i=0;i<4;i++) {
-//            							   System.out.println(data.get(i)[0]);// 0th line object
-//        }
+        for(int i=0;i<4;i++) {
+            System.out.println(data.get(i)[0]);// 0th line object
+        }
 
 
         for(String  arr[]: data){
-           			 System.out.println(arr[0]);
+            System.out.println(arr[0]);
         }
 
-//        data.stream()
-//               				 .forEach(x-> System.out.println(x[0]));
-    }	
+        data.stream()
+                .forEach(x-> System.out.println(x[0]));
+    }
 
 }
 
