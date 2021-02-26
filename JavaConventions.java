1314e6bac62478f9e76d053fb7052bfefdb1c5b9
rahulsajekar <rahulsajekar93@gmail.com>
2021-02-26 15:50:06 +0530

    Java Convention File corrected

--- JavaConventions.java
+++ JavaConventions.java
@@ -11,36 +11,33 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class ReadingCSV {
- public static void main(String[] args) throws IOException, CsvException {
+    public static void main(String[] args) throws IOException, CsvException {
         Reader reader= Files.newBufferedReader(Paths.get("Student.txt"));
 
         //parser!
-   CSVParser Parser=new CSVParserBuilder()
+        CSVParser parser=new CSVParserBuilder()
                 .withSeparator('\t')
                 .build();
 
-
-    CSVReader Obj1=new CSVReaderBuilder(reader)
+        CSVReader obj=new CSVReaderBuilder(reader)
                 .withCSVParser(parser)
-                			.build();
-
+                .build();
 
         //read the contents of the file!
+        List<String[]> data=obj.readAll(); //returns a list of Strings from the file
 
-        		List<String[]> data=obj.readAll(); //returns a list of Strings from the file
-//3 ways for using data
+        //3 ways for using data
 
-//        for(int i=0;i<4;i++) {
-//            							   System.out.println(data.get(i)[0]);// 0th line object
-//        }
-
-
-        for(String  arr[]: data){
-           			 System.out.println(arr[0]);
+        for(int row=0;row<4;row++) {
+            System.out.println(data.get(row)[0]);// 0th line object
         }
 
-//        data.stream()
-//               				 .forEach(x-> System.out.println(x[0]));
+        for(String[] row: data){
+            System.out.println(row[0]);
+        }
+
+        data.stream()
+                .forEach(row-> System.out.println(row[0]));
     }	
 
 }
